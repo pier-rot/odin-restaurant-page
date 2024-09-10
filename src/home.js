45c1ebcbeb736176c63ed5ba5645706807b9a945
Pierre V <6772696d@gmail.com>
2024-09-10 16:35:28 +0200

    Implement the makeHome function which returns the desired contents for the page when the home tab is open

--- src/home.js
+++ src/home.js
@@ -1,5 +1,20 @@
 function makeHome(){
+    const contentDiv = document.getElementById("content");
+    const containerDiv = document.createElement("div");
+    const valhallaP = document.createElement("p");
+    const cuisineP = document.createElement("p");
+    
+    containerDiv.setAttribute("id", "container");
+    valhallaP.setAttribute("id", "name");
+    cuisineP.setAttribute("id", "cuisine");
 
+    valhallaP.innerText = "valhalla";
+    cuisineP.innerText = "norse cuisine";
+
+    containerDiv.appendChild(valhallaP);
+    containerDiv.appendChild(cuisineP);
+
+    return containerDiv;
 };
 
 export {makeHome};
