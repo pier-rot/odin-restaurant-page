0b213f80e3b42d0331861fb082b52194dd4f7032
Pierre V <6772696d@gmail.com>
2024-09-10 16:39:11 +0200

    Assign variables for each tab & Add a function to empty the container

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ import { makeContact } from "./contact";
 import { makeHome } from "./home";
 import { makeMenu } from "./menu";
 import { makeReservation } from "./reservation";
+
+
+const homeDiv = makeHome();
+const aboutDiv = makeAbout();
+const contactDiv = makeContact();
+const menuDiv = makeMenu();
+const reservationDiv = makeReservation();
+const containerDiv = document.getElementById("container");
+
+function emptyContainer() {
+    // Remove the contents of the main container (containerDiv).
+    containerDiv.innerHTML = "";
+};
+
+
+// By default the container is filled with the home tab.
+containerDiv.appendChild(homeDiv);
