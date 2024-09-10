c4a546ea3a637eef1d74f1d1895602a95ebd0cad
Pierre V <6772696d@gmail.com>
2024-09-10 21:05:58 +0200

    Remove reservation pop up/button

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { makeAbout } from "./about";
 import { makeContact } from "./contact";
 import { makeHome } from "./home";
 import { makeMenu } from "./menu";
-import { makeReservation } from "./reservation";
 
 
 // Create the containers that will be put in the content
@@ -11,7 +10,6 @@ const homeDiv = makeHome();
 const aboutDiv = makeAbout();
 const contactDiv = makeContact();
 const menuDiv = makeMenu();
-const reservationDiv = makeReservation();
 
 // Selecting the content div
 const contentDiv = document.getElementById("content");
@@ -31,13 +29,11 @@ const homeBtn = document.querySelector("#homeBtn");
 const aboutBtn = document.querySelector("#aboutBtn");
 const menuBtn = document.querySelector("#menuBtn");
 const contactBtn = document.querySelector("#contactBtn");
-const bookBtn = document.querySelector("#bookBtn");
 
 homeBtn.addEventListener("click",() => {fillContent()});
 aboutBtn.addEventListener("click",() => {fillContent(aboutDiv)});
 menuBtn.addEventListener("click",() => {fillContent(menuDiv)});
 contactBtn.addEventListener("click", () => {fillContent(contactDiv)});
-bookBtn.addEventListener("click", makeReservation);
 
 
 // By default, fill page with the homeDiv on reload
--- src/reservation.js
+++
@@ -1,5 +0,0 @@
-function makeReservation() {
-
-};
-
-export {makeReservation};
